2dda8028be85cc12495c7528d473ae747a3e37c1
VincentViers <vincent.viers@sciencespo.fr>
2019-03-04 11:34:38 +0000

    refactored and comment

--- code/PSPSO.py
+++ code/PSPSO.py
@@ -1,21 +1,13 @@
-# Parallel PSO (Synchronous)
-
-import multiprocessing
-
+# import two useful libraries/modules
 import numpy as np
 import random
+from multiprocessing import Pool
 
-# -- global parameters
-c1 = 1 # cognitive parameter
-c2 = 2 # social parameter
-w = .5 # inertia
-
-# -- Particle Class
-
+#--------------------------------------------------------------------------------------------------------
 class Particle:
     """A Python Class for a simple particle."""
     
-    def __init__(self, upper, lower, ndim):
+    def __init__(self, upper, lower, ndim, c1 = 1, c2 = 2, w = .5):
         '''Initiate a Particle with an upper and lower bound, and a number of dimensions.'''
         # Initiate position and velocity randomly
         self.position = np.array([random.uniform(lower, upper) for _ in range(ndim)])
@@ -23,52 +15,86 @@ class Particle:
         # These attributes are here to store the "memory" of the function
         self.personal_best_position = self.position # initial position
         self.personal_best_error = np.inf # infinity
+        self.c1 = c1
+        self.c2 = c2
+        self.w = w
+    
     
     def __str__(self):
         '''what will be called when you print a Particle instance, could be useful for debugging.'''
         return("I am a particle with best position {},\ncurrent position {},\nand velocity {}.\n"
-               .format(self.personal_best_position, self.position, self.velocity))       
+               .format(self.personal_best_position, self.position, self.velocity))
+    
     
     def update_velocity(self, global_best):
         '''update a particle's velocity'''
         # two random coefficients in uniform([0, 1])
         r1 = random.random()
         r2 = random.random()
-        self.velocity = (w*self.velocity + # inertia
-                         c1*r1*(self.personal_best_position - self.position) + # cognitive term
-                         c2*r2*(global_best - self.position)) # social term
+        self.velocity = (self.w*self.velocity + # inertia
+                         self.c1*r1*(self.personal_best_position - self.position) + # cognitive term
+                         self.c2*r2*(global_best - self.position)) # social term
+        
             
     def move(self):
         '''Moves a Particle at a new iteration. Deals with out-of-bound cases (are they ok or not?)'''
         self.position = self.position + self.velocity
         # need to deal with when the particle goes out of bound...
-        
-        
-# --- Solver Class
-class PSO_parallel:    
-    """Parallel Solver instance for Particle Swarm Optimizer. (Basically the mastermind which commands the Particles.)
-    Initiated with:
-    - num_particles: how many particles to use?
-    - function: function to optimize.
-    - n_iter: number of iterations to be performed.
-    - lower, upper: lower and upper bounds for the search space (for now, a box i.e. they are the same accross all dimensions.
-    - ndim: dimensionality of the search space.)"""
+
+# ----------------------------------------------------------------------------------------------------------------
+class PSO:
+    """
+    +----------------------------------------------------------+
+    + Parallel Solver instance for Particle Swarm Optimizer.   +
+    + (Basically the mastermind which commands the Particles.) +
+    +----------------------------------------------------------+
     
-    def __init__(self, num_particles, function, n_iter, lower = -10, upper = 10, ndim = 3):
+    Instantiated with:
+    -----------------
+    - num_particles: how many particles to use?
+    
+    - function: function to minimize. IT MUST BE of the form:
+                function np.array[coordinates] --- f() ---> [list] (or np.array)
+                typically, the function returns a list comprehension.
+                
+    - n_iter: number of iterations to be performed. To be replaced/completed with convergence criterions.
+    
+    - ndim: dimensionality of the search space.
+    
+    - lower, upper: lower and upper bounds for the search space
+    (for now, it's a box i.e. they are the same accross all dimensions.)
+    
+    - c1, c2, w: cognitive, social, and inertia parameters. To be tuned to the specific problem.
+    
+    - parallel: whether to evaluate the fitness of particles in parallel. `False` by default as a speed-boost is
+    unlikely in most simple settings.
+    
+    """
+    
+    def __init__(self, num_particles, function, n_iter, ndim, lower = -10, upper = 10,
+                 c1 = 1, c2 = 2, w = .5, parallel = False):
         '''Initiate the solver'''
         # create all the Particles, stored in a list.
-        self.particles = [Particle(lower, upper, ndim) for _ in range(num_particles)]
+        self.particles = [Particle(lower, upper, ndim, c1, c2, w) for _ in range(num_particles)]
         self.fitnesses = np.array([])
         # store global best and associated error
         self.global_best = np.array([])
         self.global_best_error = np.inf # infinity
         self.function = function # function to be optimised
         self.n_iter = n_iter # num of iterations
+        self.parallel = parallel
+        if parallel:
+            self.pooler = Pool(multiprocessing.cpu_count() - 1)
         
     def get_fitnesses(self):
-        with Pool(2) as p:
-            fitnesses = p.apply(self.function, [[part.position for part in self.particles]])
-        self.fitnesses = np.array(fitnesses)
+        # Evaluate all fitnesses in parallel, or not.
+        if self.parallel:
+            fitnesses = self.pooler.apply(self.function, [[part.position for part in self.particles]])
+            self.fitnesses = np.array(fitnesses)
+            
+        else :
+            fitnesses = self.function([part.position for part in self.particles])
+            self.fitnesses = np.array(fitnesses)
     
     def update_particles(self):
         '''update particle best known personal position'''
@@ -94,22 +120,27 @@ class PSO_parallel:
         return """Current best position: {}
         With error: {}""".format(self.global_best, self.global_best_error)
     
-    def go(self):
+    def run(self, verbose = True):
         '''Run the algorithm and print the result. By default, update us every 50 iterations.'''
-        print("Running the PSO algorithm in parallel with {} particles, for {} iterations.\n".
-              format(len(self.particles), self.n_iter))
+        
+        if verbose:
+            print("Running the PSO algorithm in parallel with {} particles, for {} iterations.\n".
+                  format(len(self.particles), self.n_iter))
         
         for iteration in range(self.n_iter):
             # this happens in parallel (synchronous only rn)
-            pso.get_fitnesses()
+            self.get_fitnesses()
             # this doesn't
-            pso.update_particles()
-            pso.update_best()
-            pso.move_particles()
+            self.update_particles()
+            self.update_best()
+            self.move_particles()
             
-            if iteration % 50 == 0:
+            if (iteration % 50 == 0) & verbose==True:
                 print("Iteration number " + str(iteration))
                 print("Current best error: " + str(self.global_best_error))
                 print("\n")
                 
-        print("Found minimum at {} with value {}.".format(self.global_best, self.global_best_error))
+        if verbose:
+            print("Found minimum at {} with value {}.".format(self.global_best, self.global_best_error))
+            
+        return(self.global_best)
